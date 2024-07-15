09a3754607ab3b6c7a4f4549d82c272ce61e3b5a
R0yalCode <ivanjima9@gmail.com>
2024-07-15 08:16:56 -0500

    Update laberinto.c

--- laberinto.c
+++ laberinto.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#define fila 10
+#include <stdio.h>  // Libreria utilizada para la entrada y salida de datos.
+#define fila 10  // Definir la dimension de la matriz, la cual va a hacer de 10x10 
 #define colum 10
 
 // Función para limpiar la pantalla
@@ -38,3 +38,30 @@ printf("¡Bienvenido al Laberinto!\n\n");
         printf("\n");
     }
 
+printf("\nInstrucciones de movimiento:\n");
+    printf("a - Mover ratón hacia la izquierda\n");
+    printf("w - Mover ratón hacia arriba\n");
+    printf("s - Mover ratón hacia abajo\n");
+    printf("d - Mover ratón hacia la derecha\n");
+    printf("q - Salir del laberinto\n");
+    char direccion;
+    while (1) {  // Bucle infinito para movimiento continuo
+        printf("\nraton: ");
+        scanf(" %c", &direccion);
+
+        clear_screen();
+        printf("raton:\n");
+
+        // Actualizar posición del ratón según la dirección ingresada y validar movimiento
+        if (direccion == 'w' && raton_x > 0 && laberinto[raton_x - 1][raton_y] == 0) {
+            raton_x--;
+        } else if (direccion == 'a' && raton_y > 0 && laberinto[raton_x][raton_y - 1] == 0) {
+            raton_y--;
+        } else if (direccion == 's' && raton_x < fila - 1 && laberinto[raton_x + 1][raton_y] == 0) {
+            raton_x++;
+        } else if (direccion == 'd' && raton_y < colum - 1 && laberinto[raton_x][raton_y + 1] == 0) {
+            raton_y++;
+        } else if (direccion == 'q') {
+            printf("Saliendo del laberinto...\n"); 
+        } else {
+            printf("Movimiento no válido.\n");
