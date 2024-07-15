8d148fd57cdf48e82bc962d1755a1fd87cc1e7d0
MariaCVR <maria.valarezo@unl.edu.ec>
2024-07-15 08:08:14 -0500

    Update laberinto.c

--- laberinto.c
+++ laberinto.c
@@ -20,3 +20,21 @@ int main() {
         {1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
         {1, 1, 1, 1, 1, 1, 0, 0, 1, 1}
     };
+printf("¡Bienvenido al Laberinto!\n\n");
+    int raton_x = 0, raton_y = 1;  // Posición inicial del ratón
+
+    clear_screen();
+    // Mostrar laberinto inicial con posición del ratón
+    for (int i = 0; i < fila; i++) {
+        for (int j = 0; j < colum; j++) {
+            if (i == raton_x && j == raton_y) {
+                printf("R ");  // Representación del ratón
+            } else if (laberinto[i][j] == 1) {
+                printf("0");  // Pared
+            } else {
+                printf(" ");  // Espacio libre
+            }
+        }
+        printf("\n");
+    }
+
